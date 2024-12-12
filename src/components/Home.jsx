86df65cf4e1078f5fe468afec64b4293e9f1110b
koustav1002 <halder600koustav@gmail.com>
2024-12-12 12:23:09 +0530

    Changed 'View Work' functionality

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -21,14 +21,14 @@ const Home = () => {
         </p>
 
         <div>
-          <button className="text-white group border-2 px-6 py-3 my-2 flex items-center hover:bg-pink-600 hover:border-pink-600 duration-500">
-            <Link to="work" smooth={true} duration={500}>
+          <Link to="work" smooth={true} duration={500}>
+            <button className="text-white group border-2 px-6 py-3 my-2 flex items-center hover:bg-pink-600 hover:border-pink-600 duration-500">
               View Work
-            </Link>
-            <span className="group-hover:rotate-90 duration-300">
-              <HiArrowNarrowRight className="ml-3" />
-            </span>
-          </button>
+              <span className="group-hover:rotate-90 duration-300">
+                <HiArrowNarrowRight className="ml-3" />
+              </span>
+            </button>
+          </Link>
         </div>
       </div>
     </div>
